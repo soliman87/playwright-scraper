415795be8ff39225fae42ecd8a26852ce3b0e53b
soliman87 <mohamedsoliman8787@gmail.com>
2026-03-31 13:12:22 +0200

    Update exporter.py

--- src/exporter.py
+++ src/exporter.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import os
 
 def export_to_csv(data, path="data/output.csv"):
+    os.makedirs(os.path.dirname(path), exist_ok=True)  # <-- FIX
     df = pd.DataFrame(data)
     df.to_csv(path, index=False)
